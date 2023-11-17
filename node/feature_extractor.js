6de32b39551af519bfad1f577d72463275aa01c9
Randy Flyte <877wrangler@gmail.com>
2023-11-16 23:48:49 -0500

    Add data reading and display

---
+++ node/feature_extractor.js
@@ -0,0 +1,12 @@
+const constants = require('../common/constants.js');
+const features = require('../common/features.js');
+
+const fs = require('fs');
+
+const samples = JSON.parse(fs.readFileSync(constants.SAMPLES));
+
+for (const sample of samples) {
+  const paths = JSON.parse(
+    fs.readFileSync(constants.JSON_DIR + '/' + sample.id + '.json')
+  );
+}
