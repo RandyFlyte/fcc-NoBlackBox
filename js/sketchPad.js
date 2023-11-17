79f292402a4024d75aa243a81119762568a62593
Randy Flyte <877wrangler@gmail.com>
2023-11-16 21:05:34 -0500

    Add mobile functionality

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -9,6 +9,12 @@ class SketchPad {
     `;
     container.appendChild(this.canvas);
 
+    const lineBreak = document.createElement('br');
+    container.appendChild(lineBreak);
+
+    this.undoBtn = document.createElement('button');
+    this.undoBtn.innerHTML = 'UNDO';
+
     this.ctx = this.canvas.getContext('2d');
 
     this.paths = [];
@@ -33,6 +39,19 @@ class SketchPad {
     };
     this.canvas.onmouseup = () => {
       this.isDrawing = false;
+    };
+    this.canvas.ontouchstart = (evt) => {
+      evt.preventDefault();
+      const loc = evt.touches[0];
+      this.canvas.onmousedown(loc);
+    };
+    this.canvas.ontouchmove = (evt) => {
+      evt.preventDefault();
+      const loc = evt.touches[0];
+      this.canvas.onmousemove(loc);
+    };
+    this.canvas.ontouchend = () => {
+      this.canvas.onmouseup();
     };
   }
 
